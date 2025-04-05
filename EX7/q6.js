dcb45955669073315b19ceb0124f8b27f28681a0
Janhavi-Srinivasan <janhavi.srinivasan24@gmail.com>
2025-04-05 18:15:46 +0530

    Delete EX7 directory

--- EX7/q6.js
+++
@@ -1,8 +0,0 @@
-function calculateTotal() {
-    let adultTickets = document.getElementById("adults").value || 0;
-    let childTickets = document.getElementById("children").value || 0;
-    adultTickets = parseInt(adultTickets);
-    childTickets = parseInt(childTickets);
-    let total = (adultTickets * 10) + (childTickets * 5);
-    document.getElementById("totalAmount").value = `₹${total}`;
-}
--- EX7/reg.js
+++
@@ -1,9 +0,0 @@
-function checkStudent() {
-    let regNumber = prompt("Please enter your registration number:");
-    let isStudent = confirm("Are you a student?");
-    if (isStudent) {
-        alert(`Your registration number is ${regNumber}. You are confirmed as a student.`);
-    } else {
-        alert(`Your registration number is ${regNumber}. You are not a student.`);
-    }
-}
