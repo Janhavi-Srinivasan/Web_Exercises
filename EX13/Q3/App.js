6204894d54120794617cfbc9c8e4fc85c8749a7b
Janhavi-Srinivasan <janhavi.srinivasan24@gmail.com>
2025-04-06 23:30:53 +0530

    Create

---
+++ EX13/Q3/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import StyledMessage from "./StyledMessage";  
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <StyledMessage />
+    </div>
+  );
+}
+export default App;
