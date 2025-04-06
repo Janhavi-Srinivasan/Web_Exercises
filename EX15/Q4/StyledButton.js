1707442ce40f3a08da7c869748cda4293a1efe0a
Janhavi-Srinivasan <janhavi.srinivasan24@gmail.com>
2025-04-06 23:49:09 +0530

    Create

---
+++ EX15/Q4/StyledButton.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './styles.css';
+const StyledButton=({children})=>{
+    return <button className="styled-button">{children}</button>
+};
+export default StyledButton;
