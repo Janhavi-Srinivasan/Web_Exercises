7ec932092e9c9e4c69d501449ce5a6b742909755
Janhavi-Srinivasan <janhavi.srinivasan24@gmail.com>
2025-04-06 23:59:41 +0530

    Create

---
+++ EX15/Q11/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Child from "./Child";
+const App = () => {
+  const message = "Hello from Parent Component!";
+  
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Parent Component</h1>
+      <Child message={message} />
+    </div>
+  );
+};
+export default App;
