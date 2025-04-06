959820c40aaac90502194aeb89a61b2a54b0b61b
Janhavi-Srinivasan <janhavi.srinivasan24@gmail.com>
2025-04-06 23:29:51 +0530

    Create

---
+++ EX13/Q2/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import FruitList from "./FruitList";
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <FruitList />
+    </div>
+  );
+}
+export default App;
