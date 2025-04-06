aad9367f6a5592e0df1b1d90b5ea8bb69d5d0c7a
Janhavi-Srinivasan <janhavi.srinivasan24@gmail.com>
2025-04-06 23:41:50 +0530

    Create

---
+++ EX13/Q11/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LeapYearChecker from "./LeapYearChecker";
+
+function App() {
+    return (
+        <div>
+            <h1>Leap Year Checker</h1>
+            <LeapYearChecker />
+        </div>
+    );
+}
+
+export default App;
