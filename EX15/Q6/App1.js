8a24f0e78019ff6086629e39d7784048db8c48ea
Janhavi-Srinivasan <janhavi.srinivasan24@gmail.com>
2025-04-06 23:53:27 +0530

    Create

---
+++ EX15/Q6/App1.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CounterUsingReducer from "./components/Counter1";  
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Counter with useState and useReducer</h1>
+      <CounterUsingReducer />
+    </div>
+  );
+};
+
+export default App;
