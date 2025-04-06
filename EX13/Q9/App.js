4b693dcf38eeb599690d25d9c4e0df877182e5ff
Janhavi-Srinivasan <janhavi.srinivasan24@gmail.com>
2025-04-06 23:38:31 +0530

    Create

---
+++ EX13/Q9/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReverseString from "./ReverseString";
+
+function App() {
+    return (
+        <div>
+            <h1>String Reversal & Palindrome Check</h1>
+            <ReverseString inputString="React" />
+        </div>
+    );
+}
+
+export default App;
