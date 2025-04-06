488e76f3be09907910475f0a027ca4595984b640
Janhavi-Srinivasan <janhavi.srinivasan24@gmail.com>
2025-04-07 00:01:20 +0530

    Create

---
+++ EX15/Q12/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import FormWithState from "./components/FormWithState";
+import FormWithRef from "./components/FormWithRef";
+const App = () => {
+  return (
+    <div>
+      <h1>React Form Examples</h1>
+      <FormWithState />
+      <FormWithRef />
+    </div>
+  );
+};
+export default App;
