17cd6018e30ee0f7ba2a1f65f6e84bdd70f36e20
Janhavi-Srinivasan <janhavi.srinivasan24@gmail.com>
2025-04-06 23:54:57 +0530

    Create

---
+++ EX15/Q7/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import JokeFetcher from "./components/JokeFetcher";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Random Joke Generator</h1>
+      <JokeFetcher />
+    </div>
+  );
+};
+
+export default App;
