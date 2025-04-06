e64b967dd7b699009d082133d8cecf6a3548a19b
Janhavi-Srinivasan <janhavi.srinivasan24@gmail.com>
2025-04-06 23:39:42 +0530

    Create

---
+++ EX13/Q10/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import RandomNumber from "./RandomNumber";
+
+function App() {
+    return (
+        <div>
+            <h1>Random Number Generator</h1>
+            <RandomNumber />
+        </div>
+    );
+}
+
+export default App;
