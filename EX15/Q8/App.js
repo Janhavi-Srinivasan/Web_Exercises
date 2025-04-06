4bb96d15ba3951d90e9d5691c5bf6ba07fe7f4a7
Janhavi-Srinivasan <janhavi.srinivasan24@gmail.com>
2025-04-06 23:55:57 +0530

    Create

---
+++ EX15/Q8/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FocusForm from "./components/FocusForm";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Focus Input Example</h1>
+      <FocusForm />
+    </div>
+  );
+};
+export default App;
