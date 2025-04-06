439e0fc210e49b5e5ef058a99cdc4a7e67e22f85
Janhavi-Srinivasan <janhavi.srinivasan24@gmail.com>
2025-04-06 23:44:54 +0530

    Create

---
+++ EX15/Q1/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer style={{ backgroundColor: "#282c34", color: "white", padding: "10px", textAlign: "center", position: "absolute", bottom: 0, width: "100%" }}>
+      <p>&copy; 2025 My React App. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
