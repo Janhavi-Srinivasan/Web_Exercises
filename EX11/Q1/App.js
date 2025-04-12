6482f62a2eca4f0e08e5bd1e478a298dd5980a1e
Janhavi-Srinivasan <janhavi.srinivasan24@gmail.com>
2025-04-12 10:58:06 +0530

    Created

---
+++ EX11/Q1/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import DisplayImages from './components/DisplayImages';
+function App() {
+    return (
+        <div>
+            <DisplayImages />
+        </div>
+    );
+}
+export default App;
