90802c8efddcd55b02b7999babe410add90fcebe
Janhavi-Srinivasan <janhavi.srinivasan24@gmail.com>
2025-04-12 11:23:09 +0530

    Created

---
+++ EX14/Q3/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ProfilePage from "./ProfilePage";
+
+function App() {
+    return (
+        <div>
+            <h1>User Profile</h1>
+            <ProfilePage />
+        </div>
+    );
+}
+
+export default App;
